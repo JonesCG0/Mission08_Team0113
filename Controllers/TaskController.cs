b52e383a667a86f99b8966f6d796ff2f298920ec
JonesCG0 <153124541+JonesCG0@users.noreply.github.com>
2026-02-25 09:42:32 -0700

    Add TasksController skeleton and routes

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission08.Controllers
+{
+    public class TasksController : Controller
+    {
+        // Inject repository when teammate #1 has it ready
+        // private readonly ITaskRepository _repo;
+        // public TasksController(ITaskRepository repo) => _repo = repo;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // TODO: return View(tasksNotCompleted);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // TODO: ViewBag.Categories = _repo.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(object task /* replace with TaskItem */)
+        {
+            // TODO: validate + _repo.AddTask(task) + _repo.Save()
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // TODO: load task + categories, return View("Add", task)
+            return View("Add");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(object task /* replace with TaskItem */)
+        {
+            // TODO: validate + _repo.UpdateTask(task) + _repo.Save()
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            // TODO: _repo.DeleteTask(id) + _repo.Save()
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Complete(int id)
+        {
+            // TODO: set Completed=true + save
+            return RedirectToAction("Index");
+        }
+    }
+}
